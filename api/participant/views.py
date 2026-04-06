ee06d2d711425d9d4bfe107863ea773901e70af9
amit <amit210905@gmail.com>
2026-04-06 19:19:52 +0600

    Ref: added API for email existence for participant.

--- api/participant/views.py
+++ api/participant/views.py
@@ -2,7 +2,7 @@ import logging
 
 from datetime import timedelta
 from django.utils import timezone
-from rest_framework import viewsets, status, mixins, serializers
+from rest_framework import viewsets, status, mixins
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
