354047743970ccae06af09b7319a92fd8d954d2d
amit <amit210905@gmail.com>
2026-04-23 12:16:51 +0600

    Ref: added is_researcher_admin to user/me endpoint and some permission refactor.

--- api/users/serializers/user_serializers.py
+++ api/users/serializers/user_serializers.py
@@ -5,9 +5,11 @@ User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     full_name = serializers.SerializerMethodField()
+    is_admin_researcher = serializers.BooleanField(read_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'full_name', 'username', 'email', 'first_name', 'last_name']
+        fields = ['id', 'full_name', 'username', 'email', 'first_name', 'last_name', 'is_admin_researcher']
 
     def get_full_name(self, obj):
         return obj.get_full_name()
